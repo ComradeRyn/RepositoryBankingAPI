c753f54e0b248eb26771619a7fc4df6954f0d7ec
ComradeRyn <ryanyuncza@gmail.com>
2026-01-16 16:57:07 -0500

    Fixed Invalid name message

--- RepositoryBankingAPI/RepositoryBankingAPI/Services/Messages.cs
+++ RepositoryBankingAPI/RepositoryBankingAPI/Services/Messages.cs
@@ -6,4 +6,6 @@ public static class Messages
     public const string InsufficientBalance = "Requested amount must be less or equal to current balance";
     public const string RequirePositiveAmount = "Requested amount must be positive";
     public const string InvalidName = "Requested amount must be less than or equal to current balance";
+    public const string InvalidCurrencies = "Invalid currency input, must follow format of " +
+                                            "<FirstType>,<...>,<FinalType>";
 }
