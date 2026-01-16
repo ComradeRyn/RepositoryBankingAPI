5072a303a8145e59316f9242e36d8c064445f35c
ComradeRyn <ryanyuncza@gmail.com>
2026-01-16 13:49:47 -0500

    Created extention methods for quick Dto transformation and status code validation

--- RepositoryBankingAPI/RepositoryBankingAPI/Services/ExtensionMethods.cs
+++ RepositoryBankingAPI/RepositoryBankingAPI/Services/ExtensionMethods.cs
@@ -1,6 +1,17 @@
-﻿namespace RepositoryBankingAPI.Services;
+﻿using System.Net;
+using RepositoryBankingAPI.Models;
+using RepositoryBankingAPI.Models.DTOs.Responses;
 
-public class ExtensionMethods
+namespace RepositoryBankingAPI.Services;
+
+public static class ExtensionMethods
 {
-    
+    public static AccountResponse AsDto(this Account account)
+        => new AccountResponse(account.Id, account.HolderName, account.Balance);
+
+    public static ChangeBalanceResponse AsDto(this decimal amount)
+        => new ChangeBalanceResponse(amount);
+
+    public static bool ValidateSuccessfulCode<T>(this ApiResponse<T> response)
+        => response.StatusCode is >= (HttpStatusCode)200 and < (HttpStatusCode)300;
 }
