6cdbddd5a265820ca4bf21d5706df59615822c0f
ComradeRyn <ryanyuncza@gmail.com>
2026-01-16 13:48:55 -0500

    Create message utility class

---
+++ RepositoryBankingAPI/RepositoryBankingAPI/Services/Messages.cs
@@ -0,0 +1,9 @@
+﻿namespace RepositoryBankingAPI.Services;
+
+public static class Messages
+{
+    public const string NotFound = "Requested object could not be found";
+    public const string InsufficientBalance = "Requested amount must be less or equal to current balance";
+    public const string RequirePositiveAmount = "Requested amount must be positive";
+    public const string InvalidName = "Requested amount must be less than or equal to current balance";
+}
