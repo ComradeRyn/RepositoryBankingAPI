7fd68984bab44ca2e673194165a9cd453d0997a0
ComradeRyn <ryanyuncza@gmail.com>
2026-01-20 15:17:53 -0500

    Created swagger dependencies

--- RepositoryBankingAPI/RepositoryBankingAPI/Program.cs
+++ RepositoryBankingAPI/RepositoryBankingAPI/Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AccountContext>(opt => opt.UseInMemoryDatabase("AccountList"));
@@ -28,6 +30,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
