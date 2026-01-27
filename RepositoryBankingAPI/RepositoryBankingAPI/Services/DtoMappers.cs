6c66a7fc4330de1ed6aca219ad64b7026c8354ad
ComradeRyn <ryanyuncza@gmail.com>
2026-01-27 11:55:30 -0500

    Renamed DtoExtentionMethods to DtoMappers

--- RepositoryBankingAPI/RepositoryBankingAPI/Services/DtoMappers.cs
+++ RepositoryBankingAPI/RepositoryBankingAPI/Services/DtoMappers.cs
@@ -3,10 +3,8 @@ using Account = RepositoryBankingAPI.Models.Account;
 
 namespace RepositoryBankingAPI.Services;
 
-public static class AsDtoExtensionMethods
+public static class DtoMappers
 {
     public static Models.DTOs.Account AsDto(this Account account) 
         => new(account.Id, account.HolderName, account.Balance);
-    public static ConversionResponse AsDto(this Dictionary<string, decimal> convertedCurrencies)
-        => new(convertedCurrencies);
 }
