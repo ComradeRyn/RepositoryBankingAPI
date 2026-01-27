be08901f3a8efdbba89a94f0b327cf40dac5da29
ComradeRyn <ryanyuncza@gmail.com>
2026-01-27 09:35:31 -0500

    Renamed Models.Account datatype to model and Account Dto to Models.DTOs.Account in AsDtoExtensionMethods

--- RepositoryBankingAPI/RepositoryBankingAPI/Services/AsDtoExtensionMethods.cs
+++ RepositoryBankingAPI/RepositoryBankingAPI/Services/AsDtoExtensionMethods.cs
@@ -1,11 +1,11 @@
 ﻿using RepositoryBankingAPI.Models.DTOs.Responses;
-using Account = RepositoryBankingAPI.Models.DTOs.Responses.Account;
+using Account = RepositoryBankingAPI.Models.Account;
 
 namespace RepositoryBankingAPI.Services;
 
 public static class AsDtoExtensionMethods
 {
-    public static Account AsDto(this Models.Account account) 
+    public static Models.DTOs.Account AsDto(this Account account) 
         => new(account.Id, account.HolderName, account.Balance);
     public static ConversionResponse AsDto(this Dictionary<string, decimal> convertedCurrencies)
         => new(convertedCurrencies);
